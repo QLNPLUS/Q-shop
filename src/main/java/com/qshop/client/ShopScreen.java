ebdb655ad50bc39d065d5d584d152a3db32215f2
qlnx2012 <qlnx2012@user.noreply.gitee.com>
2026-09-13 15:40:10 +0800

    Fix Forge shop tab scroll reset after trades

--- src/main/java/com/qshop/client/ShopScreen.java
+++ src/main/java/com/qshop/client/ShopScreen.java
@@ -174,6 +174,11 @@ public class ShopScreen extends QShopScreen {
         this.requestedServerTab = Math.max(0, activeServerTab);
     }
 
+    public ShopScreen(OpenShopPacket data, int scroll, boolean editMode, int activeServerTab, int tabScroll) {
+        this(data, scroll, editMode, activeServerTab);
+        this.tabScroll = Math.max(0, tabScroll);
+    }
+
     @Override
     protected int qshopContentWidth() {
         return TAB_BAR_W + 6 + panelWidth();
@@ -867,6 +872,10 @@ public class ShopScreen extends QShopScreen {
             return -1;
         }
         return serverTabIndex(activeTab);
+    }
+
+    int tabScroll() {
+        return tabScroll;
     }
 
     private void scrollActiveTabIntoView() {
